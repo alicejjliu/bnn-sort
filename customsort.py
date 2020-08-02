442f79aec556fa39440333cac439d732e17f9664
alicejjliu <alicejjliu@gmail.com>
2020-08-02 01:04:00 -0400

    fixed bugs, added ascending/decending selector

--- customsort.py
+++ customsort.py
@@ -3,19 +3,28 @@ import argparse
 from functools import cmp_to_key
 
 parser = argparse.ArgumentParser(description = "Sort a list of arbitrary strings using a biological neural network connected to a mechanical-electric interface")
-praser.add_argument("input", type = str, help = "input file path")
+parser.add_argument("input", type = str, help = "input file path")
 parser.add_argument("-o", "--output", type = str, help = "output file path")
+parser.add_argument("-a", "--ascending", action = "store_false")
 
+cmpdict = {}
 def compare(a, b):
+    if ((a, b) in cmpdict):
+        return cmpdict[(a, b)]
+    if ((b, a) in cmpdict):
+        return -cmpdict[(b, a)]
     user_in = ""
     valid_in = ("1", "2", "3")
-    while (user_in not in valid_in)
-        user_in = input(("Is " + a + " better than " + b + "? (1 if yes, 2 if no, 3 if they're the same)"))
+    while (user_in not in valid_in):
+        user_in = input(("Is " + a + " better than " + b + "? (1 if yes, 2 if no, 3 if they're the same): "))
     if (user_in == "1"):
+        cmpdict[(a, b)] = 1
         return 1
     elif (user_in == "2"):
+        cmpdict[(a, b)] = -1
         return -1
     elif (user_in == "3"):
+        cmpdict[(a, b)] = 0
         return 0
     return 0
 
@@ -25,7 +34,7 @@ inlist = []
 with open(args.input, "r") as infile:
     for line in infile:
         inlist.append(line.rstrip())
-outlist = sorted(inlist, key = cmp_to_key(compare))
+outlist = sorted(inlist, key = cmp_to_key(compare), reverse = args.ascending)
 
 if (args.output is None):
 #We output to stdout the sorted list
